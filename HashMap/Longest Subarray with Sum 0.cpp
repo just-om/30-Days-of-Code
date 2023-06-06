6f90701156b353438ae03e3baf01940017f9c2a4
Om Maind <100376921+just-om@users.noreply.github.com>
2023-06-06 17:40:00 +0530

    Create Longest Subarray with Sum 0.cpp

---
+++ HashMap/Longest Subarray with Sum 0.cpp
@@ -0,0 +1,30 @@
+int maxLen(vector<int>&A, int n)
+    {   
+        // Your code here
+        
+        unordered_map<int,int> mp;
+        
+        int sum=0;
+        int ans=0;
+        
+        //mp[0]=-1;
+        
+        for(int i=0;i<n;i++){
+            sum+=A[i];
+            
+            if(sum==0){
+                ans=max(ans,i+1);
+            }
+            
+            if(mp.find(sum)!=mp.end()){
+                
+               ans=max(ans,i-mp[sum]); 
+            }
+            else{
+                mp[sum]=i;
+            }
+        }
+        
+        return ans;
+        
+    }
